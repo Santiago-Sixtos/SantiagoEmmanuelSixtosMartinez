6a93714418f5f92b1be02762e4d3b2f418dd0566
Santiago Sixtos <menny_2208@hotmail.com>
2023-11-16 12:15:00 -0600

    16/11/2023

--- Parcial3/ServerExpress/Index.js
+++ Parcial3/ServerExpress/Index.js
@@ -1,16 +1,16 @@
 const express=require('express');
 const app=express('cors');
-const cors = require('cors')
+const cors = require('cors');
+
 app.get('/',(req,res)=>{
     res.json({mensaje:"Server express respondiendo a get"});
 });
-
 app.post('/',(req,res)=>{
-    res.json({mensaje:"Server express respondiendo a post"})
-})
+    res.json({mensaje:"Server express respondiendo a post"});
+});
 app.delete('/',(req,res)=>{
-    res.json({mensaje:"Server express respondiendo a delete"})
-})
+    res.json({mensaje:"Server express respondiendo a delete"});
+});
 app.listen(8082,()=>{
     console.log("Servidor express en puerto 8082")
 });
