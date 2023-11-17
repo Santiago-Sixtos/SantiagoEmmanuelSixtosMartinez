4033b0639fef1a75582b880f9c3fec47d1bbb2fa
Santiago Sixtos <menny_2208@hotmail.com>
2023-11-17 07:41:01 -0600

    17/11/2023

--- Parcial3/ServerExpress/Index.js
+++ Parcial3/ServerExpress/Index.js
@@ -1,6 +1,7 @@
-const express=require('express');
-const app=express('cors');
+const express = require('express');
+const app = express('cors');
 const cors = require('cors');
+// app.use=(cors());
 
 app.get('/',(req,res)=>{
     res.json({mensaje:"Server express respondiendo a get"});
